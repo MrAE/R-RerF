a7f295f9b79e87ed4d3d35b5da66976ea3f9bb1d
Jesse Leigh Patsolic <StudioJLP@gmail.com>
2018-04-20 15:59:00 -0400

    add image-patch-bessl option

--- R/RandMat.R
+++ R/RandMat.R
@@ -10,6 +10,8 @@
 #'
 #' @importFrom RcppZiggurat zrnorm
 #' @importFrom stats rpois runif
+#' @importFrom gsl bessel_Jn 
+#' @importFrom raster resample raster
 
 RandMat <-
 function(mat.options) {
@@ -103,6 +105,44 @@ function(mat.options) {
     }
     # random.matrix <- cbind(nz.rows, nz.cols, sample(c(-1L,1L), nnz, replace = T))
     random.matrix <- cbind(nz.rows, nz.cols, rep(1L, nnz))
+  } else if (method == "image-patch-bessel") {
+    iw <- mat.options[[4L]] # image width
+    ih <- mat.options[[5L]] # image height
+    pw.min <- mat.options[[6L]] # minimum patch width
+    pw.max <- mat.options[[7L]] # maximum patch width
+    pw <- sample.int(pw.max - pw.min + 1L, 2*d, replace = T) + pw.min - 1L
+    sample.height <- ih - pw[1:d] + 1L
+    sample.width <- iw - pw[(d + 1L):(2*d)] + 1L
+    nnz <- sum(pw[1:d]*pw[(d + 1L):(2*d)])
+    nz.rows <- integer(nnz)
+    nz.cols <- integer(nnz)
+    start.idx <- 1L
+    for (i in seq.int(d)) {
+      top.left <- sample.int(sample.height[i]*sample.width[i], 1L)
+      top.left <- floor((top.left - 1L)/sample.height[i])*(ih - sample.height[i]) + top.left
+      # top.left <- floor((top.left - 1L)/sample.height[i]) + top.left
+      end.idx <- start.idx + pw[i]*pw[i + d] - 1L
+      nz.rows[start.idx:end.idx] <- sapply((1:pw[i + d]) - 1L, function(x) top.left:(top.left + pw[i] - 1L) + x*ih)
+      nz.cols[start.idx:end.idx] <- i
+      start.idx <- end.idx + 1L
+    }
+    # random.matrix <- cbind(nz.rows, nz.cols, sample(c(-1L,1L), nnz, replace = T))
+    bkm <- function(x,y, k = 1, nu = 0){
+      r <- sqrt(x^2 + y^2)
+  	  Re(sqrt(k) * bessel_Jn(nu, k * r) *  1/sqrt(2*pi) * exp(1i * nu * atan2(y,x)))
+  	}
+    tmp <- cbind(nz.rows, nz.cols, rep(1L, nnz))
+    
+    if (nz.rows >= 4 && nz.cols >= 4) {
+      tmp.raster <- raster(tmp)
+      b <- 8
+      bb <- 0.1
+      bes.0 <- bkm(seq(-b,b,bb),seq(-b,b,bb), k = 1, nu = 0)
+      bes.mat <- as.matrix(raster::resample(raster(bes.mat), tmp.raster))
+      random.matrix <- bes.mat * as.matrix(tmp)
+    } else {
+      random.matrix <- tmp
+    }
   } else if (method == "image-control") {
     iw <- mat.options[[4L]] # image width
     ih <- mat.options[[5L]] # image height
