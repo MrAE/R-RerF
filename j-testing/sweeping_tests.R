e9174447929dcf58f871f455f283f029022546ff
Jesse Leigh Patsolic <StudioJLP@gmail.com>
2018-04-23 14:53:40 -0400

    init for MARCC run.

---
+++ j-testing/sweeping_tests.R
@@ -0,0 +1,18 @@
+###
+### ### INITIAL COMMENTS HERE ###
+###
+### Jesse Leigh Patsolic 
+### 2018 <jpatsol1@jhu.edu>
+### S.D.G 
+#
+
+
+require(rerf)
+
+
+
+
+#   Time:
+##  Working status:
+### Comments:
+####Soli Deo Gloria
