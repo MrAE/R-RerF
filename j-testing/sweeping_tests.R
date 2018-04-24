2f4d3798324c89ce47218df8afdfa71ef87497bf
Jesse Leigh Patsolic <StudioJLP@gmail.com>
2018-04-24 16:33:29 -0400

    ready for run1.

--- j-testing/sweeping_tests.R
+++ j-testing/sweeping_tests.R
@@ -8,26 +8,38 @@
 
 
 require(rerf)
-data(mnist)
+install.packages('doMC')
+require(doMC)
+registerDoMC(4)
+#data(mnist)
+mnist <- readRDS('../R/mnist.Rdat')
 
-# p is number of dimensions, d is the number of random features to evaluate, iw is image width, ih is image height, patch.min is min width of square patch to sample pixels from, and patch.max is the max width of square patch
+# p is number of dimensions, d is the number of random features to
+# evaluate, iw is image width, ih is image height, patch.min is min
+# width of square patch to sample pixels from, and patch.max is the max
+# width of square patch
 p <- ncol(mnist$Xtrain)
 d <- ceiling(sqrt(p))
 iw <- sqrt(p)
 ih <- iw
 patch.min <- 1L
 patch.max <- 5L
+pmin.max <- expand.grid(1:4, 2:5)
 
+opt.rf <- lapply(1:28, function(d) list(p, d, "rf"))
+opt.poisson <- lapply(1:10, function(lam) list(p, d, "poisson", lambda = lam))
+opt.image.patch <- lapply(1:nrow(pmin.max), function(i) { list(p, d, "image-patch", iw, ih, patch.min = pmin.max[i, 1], patch.max = pmin.max[i, 2]) })
 
+opt.list <- c(opt.rf, opt.poisson, opt.image.patch)
 
+run1 <- foreach(oi = opt.list) %dopar% {
+  forest <- RerF(mnist$Xtrain, mnist$Ytrain, num.cores = 1L, mat.options = oi, seed = 1L)
+  predictions <- Predict(mnist$Xtest, forest, num.cores = 1L)
+  error.rate <- mean(predictions != mnist$Ytest)
+  list(forest = forest, pred = predictions, error = error.rate)
+}
 
-forest <- RerF(mnist$Xtrain, mnist$Ytrain, num.cores = 1L, mat.options = list(p, d, "image-patch", iw, ih, patch.min, patch.max), seed = 1L)
-
-predictions <- Predict(mnist$Xtest, forest, num.cores = 1L)
-
-(error.rate <- mean(predictions != mnist$Ytest))
-
-
+saveRDS(run1, file = "run1.Rdat")
 
 
 
